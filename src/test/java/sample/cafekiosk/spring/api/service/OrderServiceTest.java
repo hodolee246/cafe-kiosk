286bab2b71d57bfd9ef1849eee87a7af7f02982d
hodolee246 <hodolee246@naver.com>
2024-12-15 22:51:15 +0900

    5-7 진행중

--- src/test/java/sample/cafekiosk/spring/api/service/OrderServiceTest.java
+++ src/test/java/sample/cafekiosk/spring/api/service/OrderServiceTest.java
@@ -1,11 +1,9 @@
 package sample.cafekiosk.spring.api.service;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import sample.cafekiosk.spring.api.request.OrderCreateRequest;
@@ -17,11 +15,14 @@ import sample.cafekiosk.spring.domain.product.Product;
 import sample.cafekiosk.spring.domain.product.ProductRepository;
 import sample.cafekiosk.spring.domain.product.ProductSellingStatus;
 import sample.cafekiosk.spring.domain.product.ProductType;
+import sample.cafekiosk.spring.domain.stock.Stock;
+import sample.cafekiosk.spring.domain.stock.StockRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
 
 @ActiveProfiles("test")
 @SpringBootTest
@@ -40,6 +41,9 @@ class OrderServiceTest {
     @Autowired
     private ProductRepository productRepository;
 
+    @Autowired
+    private StockRepository stockRepository;
+
     @AfterEach
     void tearDown() {
         productRepository.deleteAllInBatch();
@@ -56,8 +60,12 @@ class OrderServiceTest {
         Product product3 = createProduct(ProductType.HANDMADE, "003", 3000);
         productRepository.saveAll(List.of(product1, product2, product3));
 
+        Stock stock = Stock.create("001", 2);
+        Stock stock2 = Stock.create("002", 2);
+        stockRepository.saveAll(List.of(stock, stock2));
+
         OrderCreateRequest request = OrderCreateRequest.builder()
-                .productNumbers(List.of("001", "002"))
+                .productNumbers(List.of("001", "001", "002", "003"))
                 .build();
         //when
         LocalDateTime now = LocalDateTime.now();
@@ -66,14 +74,22 @@ class OrderServiceTest {
         assertThat(orderResponse.getOrderId()).isNotNull();
         assertThat(orderResponse)
                 .extracting("registeredDateTime", "totalPrice")
-                .contains(now, 4000);
-        assertThat(orderResponse.getProducts()).hasSize(2)
+                .contains(now, 10000);
+        assertThat(orderResponse.getProducts()).hasSize(4)
                 .extracting("productNumber", "price")
                 .containsExactlyInAnyOrder(
                         tuple("001", 1000),
-                        tuple("002", 2000)
+                        tuple("001", 1000),
+                        tuple("002", 3000),
+                        tuple("003", 5000)
                 );
-
+        List<Stock> stocks = stockRepository.findAll();
+        assertThat(stocks).hasSize(2)
+                .extracting("productNumber", "quantity")
+                .containsExactlyInAnyOrder(
+                        tuple("001", 0),
+                        tuple("002", 1)
+                );
     }
 
     @DisplayName("중복되는 상품번호 리스트로 주문을 생성할 수 있다.")
